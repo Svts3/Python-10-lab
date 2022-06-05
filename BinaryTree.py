a2e6b01efd9ae2e2b87d32dd322bd8b3f9530db8
Svyatoslav Tsar <svyatko.tsar@gmail.com>
2022-06-06 00:41:06 +0300

    Changed delete method, removed root node

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,14 +1,11 @@
 class BinaryTree:
     def __init__(self, x, y):
-        self.root = None
         self.left = None
         self.right = None
         self.x = x
         self.y = y
 
     def push(self, x, y):
-        if self.root is None:
-            self.root = BinaryTree(x, y)
         if self.y > y:
             if self.left is None:
                 self.left = BinaryTree(x, y)
@@ -22,10 +19,10 @@ class BinaryTree:
 
     def print_all_positive_y(self, root):
         if root is not None:
-            self.print_all_positive_y(root.right)
+            self.print_all_positive_y(root.left)
             if root.y > 0:
                 print(root.x, root.y, sep=", ")
-            self.print_all_positive_y(root.left)
+            self.print_all_positive_y(root.right)
 
     def top_down_tree_traversal(self, root):
         if root:
@@ -33,30 +30,55 @@ class BinaryTree:
             self.top_down_tree_traversal(root.left)
             self.top_down_tree_traversal(root.right)
 
-    def delete_nodes_with_x_and_y(self, root):
-        if root is None:
-            return None
-        if root.left is None and root.right is None:
-            return root
-        root.left = self.delete_nodes_with_x_and_y(root.left)
-        root.right = self.delete_nodes_with_x_and_y(root.right)
-        if root.left and root.left.y < 0:
-            if root.y > root.left.y and root.x > root.left.x:
-                new_root = root.right
-                temp = root
-                root = None
-                del temp
-                return new_root
-            else:
-                new_root = root.left
-                temp = root
-                root = None
-                del temp
-                return new_root
-        if root.right and root.left.y < 0 and root.left.x < 0:
-            new_root = root.left
-            temp = root
-            root = None
-            del temp
-            return new_root
-        return root
+    def get_list_of_keys(self):
+        list_of_keys = []
+        if self.y < 0 and self.x < 0:
+            list_of_keys.append(self)
+        if self.left:
+            list_of_keys += self.left.get_list_of_keys()
+        if self.right:
+            list_of_keys += self.right.get_list_of_keys()
+        return list_of_keys
+
+    def search_by_key(self, y):
+        found = []
+        if self.y == y:
+            found.append(self)
+        if self.left:
+            found += self.left.search_by_key(y)
+        if self.right:
+            found += self.right.search_by_key(y)
+        return found
+
+    def find_minimal_node(self):
+        if self.left is None:
+            return self
+        return self.left.find_minimal_node()
+
+    def delete_node(self, key):
+        node_to_delete = self.search_by_key(key)[0]
+        if node_to_delete.y < self.y:
+            if self.left:
+                self.left = self.left.delete_node(key)
+        elif node_to_delete.y > self.y:
+            if self.right:
+                self.right = self.right.delete_node(key)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            elif self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.left
+
+            min_val = self.right.find_minimal_node()
+            self.y, self.x = min_val.y, min_val.x
+            self.right = self.right.delete_node(min_val.y)
+
+        return self
+
+    def delete_all_negatives(self):
+        list_of_keys = self.get_list_of_keys()
+        for i in list_of_keys:
+            if i in list_of_keys:
+                self.delete_node(i.y)
